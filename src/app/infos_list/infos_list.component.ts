33ee1456cb24a54ac656d4dd120fbc1907138976
DUMEOU Gilles Ext OBS/OAB <PTRN4883@orange.com>
2018-02-15 17:26:23 +0100

    Ajout gestion multinews

--- src/app/infos_list/infos_list.component.ts
+++ src/app/infos_list/infos_list.component.ts
@@ -21,6 +21,47 @@ export class InfosListComponent implements OnInit{
     ngOnInit() {
     }
 
+    list_of_news = [
+        {
+             "id":5
+            ,"type":"INFOS"
+            ,"title":"La rumeur du jour : IT 's possible?"
+            ,"img":"http://www.fadeaway.fr/assets/images/news_5.jpg"
+            ,"date":"xx/xx/xxxx"
+            ,"by":"XXXXXX X."
+            ,"contains":'Voici la question et rumeur du jour ! Isaiah Thomas serait-il transférable avant la Trade deadline ?<br/>'+
+            '<br/>'+
+            'Malgré la victoire à l\'arraché de cette nuit contre Minnesota, les Cavaliers sont toujours...'
+        }
+        ,{
+            "id":4
+            ,"type":"CLUTCH"
+            ,"title":"Le facteur X du jour : Patty Mils"
+            ,"date":"xx/xx/xxxx"
+            ,"by":"XXXXXX X."
+            ,"img":"http://www.fadeaway.fr/assets/images/news_4.jpg"
+            ,"contains":""
+        }
+        ,{
+            "id":3
+            ,"type":"DOSSIERS"
+            ,"title":"Le dossier du jour : Les Toronto Raptors"
+            ,"date":"xx/xx/xxxx"
+            ,"by":"XXXXXX X."
+            ,"img":"http://www.fadeaway.fr/assets/images/news_3.jpg"
+            ,"contains":""
+        }
+        ,{
+            "id":2
+            ,"type":"INFOS"
+            ,"title":"La (mauvaise) info du jour : La blessure de K. Porzingis"
+            ,"date":"xx/xx/xxxx"
+            ,"by":"XXXXXX X."
+            ,"img":"http://www.fadeaway.fr/assets/images/news_2.jpg"
+            ,"contains":""
+        }
+    ];
+
     infos_test = 'Voici la question et rumeur du jour ! Isaiah Thomas serait-il transférable avant la Trade deadline ? ...';
 
 }
