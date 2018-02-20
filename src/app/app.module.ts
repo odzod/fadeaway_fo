de7597d325c9de25801e976d75fbcbe5195e2ed0
DUMEOU Gilles Ext OBS/OAB <PTRN4883@orange.com>
2018-02-20 17:37:46 +0100

    Ajout changement liste infos depuis api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,68 +12,9 @@ import {PlatformModule} from '@angular/cdk/platform';
 import {PortalModule} from '@angular/cdk/portal';
 import {ScrollDispatchModule} from '@angular/cdk/scrolling';
 import {CdkStepperModule} from '@angular/cdk/stepper';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
 import {CdkTableModule} from '@angular/cdk/table';
 import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-} from '@angular/material';
-import {AppComponent} from './app.component';
-import {EntryApp,MenuComponent} from './menu/menu.component';
-import {InfosListComponent} from "./infos_list/infos_list.component";
-import {InfosComponent} from "./infos/infos.component";
-import {AppRoutingModule} from "./routes";
-import { Angular2Carousel }   from './carousel/Angular2Carousel.component';
-import { Carousel } from './carousel/carousel.component';
-import { Slide } from './carousel/slide.component';
-
-/**
- * NgModule that includes all Material modules that are required to serve the app.
- */
-@NgModule({
-  exports: [
-    // CDK
-    A11yModule,
-    BidiModule,
-    ObserversModule,
-    OverlayModule,
-    PlatformModule,
-    PortalModule,
-    ScrollDispatchModule,
-    CdkStepperModule,
-    CdkTableModule,
-    
-    // Material
     MatAutocompleteModule,
     MatButtonModule,
     MatButtonToggleModule,
@@ -107,33 +48,93 @@ import { Slide } from './carousel/slide.component';
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-  ]
+} from '@angular/material';
+import {AppComponent} from './app.component';
+import {EntryApp, MenuComponent} from './menu/menu.component';
+import {InfosListComponent} from "./infos_list/infos_list.component";
+import {InfosComponent} from "./infos/infos.component";
+import {AppRoutingModule} from "./routes";
+import {Angular2Carousel} from './carousel/Angular2Carousel.component';
+import {Carousel} from './carousel/carousel.component';
+import {Slide} from './carousel/slide.component';
+
+/**
+ * NgModule that includes all Material modules that are required to serve the app.
+ */
+@NgModule({
+    exports: [
+        // CDK
+        A11yModule,
+        BidiModule,
+        ObserversModule,
+        OverlayModule,
+        PlatformModule,
+        PortalModule,
+        ScrollDispatchModule,
+        CdkStepperModule,
+        CdkTableModule,
+
+        // Material
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+    ]
 })
-export class MaterialModule {}
+export class MaterialModule {
+}
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule,
-    MaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-  ],
-  declarations: [AppComponent
-      ,EntryApp,MenuComponent,
-      InfosListComponent,
-      InfosComponent,
-      Angular2Carousel,Carousel,Slide,
-  ],
-  bootstrap: [AppComponent],
-  providers: []
+    imports: [
+        BrowserModule,
+        CommonModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    declarations: [AppComponent
+        , EntryApp, MenuComponent,
+        InfosListComponent,
+        InfosComponent,
+        Angular2Carousel, Carousel, Slide,
+    ],
+    bootstrap: [AppComponent],
+    providers: []
 })
-export class AppModule {}
-
-
-
+export class AppModule {
+}
 
 
 /**
--- src/app/infos_list/infos_list.component.ts
+++ src/app/infos_list/infos_list.component.ts
@@ -1,7 +1,9 @@
 
 import {Component, ViewEncapsulation, OnInit} from '@angular/core';
+
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import {ConfigService} from "../config";
 
 @Component({
     moduleId: module.id,
@@ -9,56 +11,24 @@ import { Router } from '@angular/router';
     templateUrl: 'infos_list.component.html',
     styleUrls: ['infos_list.component.css'],
     encapsulation: ViewEncapsulation.None,
+    providers: [ConfigService],
     preserveWhitespaces: false,
 })
 
 export class InfosListComponent implements OnInit{
 
-    constructor(private route: ActivatedRoute,private router: Router) {
-        this.route.params.subscribe(res => console.log(res.id));
+    constructor(private route: ActivatedRoute,private router: Router, private config:ConfigService) {
+        this.route.params.subscribe();
     }
 
     ngOnInit() {
+        this.config.httpGET("news/last","").subscribe(res=>
+        {
+            this.list_of_news = res['data'];
+        });
     }
 
-    list_of_news = [
-        {
-             "id":5
-            ,"type":"INFOS"
-            ,"title":"La rumeur du jour : IT 's possible?"
-            ,"img":"http://www.fadeaway.fr/assets/images/news_5.jpg"
-            ,"date":"xx/xx/xxxx"
-            ,"by":"XXXXXX X."
-            ,"contains":'Voici la question et rumeur du jour ! Isaiah Thomas serait-il transférable avant la Trade deadline ?'
-        }
-        ,{
-            "id":4
-            ,"type":"CLUTCH"
-            ,"title":"Le facteur X du jour : Patty Mils"
-            ,"date":"xx/xx/xxxx"
-            ,"by":"XXXXXX X."
-            ,"img":"http://www.fadeaway.fr/assets/images/news_4.jpg"
-            ,"contains":""
-        }
-        ,{
-            "id":3
-            ,"type":"DOSSIERS"
-            ,"title":"Le dossier du jour : Les Toronto Raptors"
-            ,"date":"xx/xx/xxxx"
-            ,"by":"XXXXXX X."
-            ,"img":"http://www.fadeaway.fr/assets/images/news_3.jpg"
-            ,"contains":""
-        }
-        ,{
-            "id":2
-            ,"type":"INFOS"
-            ,"title":"La (mauvaise) info du jour : La blessure de K. Porzingis"
-            ,"date":"xx/xx/xxxx"
-            ,"by":"XXXXXX X."
-            ,"img":"http://www.fadeaway.fr/assets/images/news_2.jpg"
-            ,"contains":""
-        }
-    ];
+    list_of_news = [];
 
     infos_test = 'Voici la question et rumeur du jour ! Isaiah Thomas serait-il transférable avant la Trade deadline ? ...';
 
